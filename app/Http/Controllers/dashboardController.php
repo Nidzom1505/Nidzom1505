26f2f28bf5c887639aa230c239545060855da8c2
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2026-03-17 11:32:54 +0700

    Fix case sensitivity: rename controllers and services to PascalCase

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -6,7 +6,7 @@ use App\Http\Services\DashboardService;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-class dashboardController extends Controller
+class DashboardController extends Controller
 {
     public function index(DashboardService $dashboardService)
     {
--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
-class projectController
+class ProjectController
 {
     public function index(Request $request)
     {
