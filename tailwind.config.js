a3e94bbf03305628811a6f40b8af2924c4b97aa8
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2026-03-17 11:44:04 +0700

    restore tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./app/Http/Controllers/**/*.php",
+        "./app/Http/Services/**/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
