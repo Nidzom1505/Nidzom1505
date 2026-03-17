9f726fd777411fb9147f1c098692704a813b1a0e
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2026-03-17 11:04:23 +0700

    Fix: bootstrap cache and storage path for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,31 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+$app->setBootstrapCachePath('/tmp/bootstrap/cache');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
