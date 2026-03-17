c5f52b382eef358ec48f1b20ea37b183e3875012
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2026-03-17 11:30:29 +0700

    Fix: Change camelCase to PascalCase for Vercel compatibility

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\dashboardController;
-use App\Http\Controllers\projectController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [dashboardController::class, 'index'])->name('dashboard');
-Route::get('/projects', [projectController::class, 'index'])->name('projects');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
