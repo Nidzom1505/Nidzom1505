fd7d805d81e800f7f5fa76cd141b106354290de2
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2026-03-27 13:31:38 +0700

    fix: ayo kunjungi my portfolio

--- resources/views/components/admin/side-bar/side-bar.blade.php
+++ resources/views/components/admin/side-bar/side-bar.blade.php
@@ -6,11 +6,40 @@
     </div>
 
     <nav class="mt-6 px-4 space-y-2">
-        <x-admin.side-bar.nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')" icon="home">Dashboard</x-admin.side-bar.nav-link>
-        <x-admin.side-bar.nav-link href="{{route('profile.edit')}}" icon="user">Profile</x-admin.side-bar.nav-link>
-        <x-admin.side-bar.nav-link href="{{route('project.edit')}}" icon="briefcase">Projects</x-admin.side-bar.nav-link>
-        <x-admin.side-bar.nav-link href="{{route('skill.edit')}}" icon="cpu">Skills</x-admin.side-bar.nav-link>
-        <x-admin.side-bar.nav-link href="{{route('experience.edit')}}" icon="map">Experience</x-admin.side-bar.nav-link>
+        <x-admin.side-bar.nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')" icon="home">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+            Dashboard
+        </x-admin.side-bar.nav-link>
+
+        <x-admin.side-bar.nav-link href="{{route('profile.edit')}}" icon="user">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+            Profile
+        </x-admin.side-bar.nav-link>
+
+        <x-admin.side-bar.nav-link href="{{route('project.edit')}}" icon="briefcase">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+            </svg>
+            Projects
+        </x-admin.side-bar.nav-link>
+
+        <x-admin.side-bar.nav-link href="{{route('skill.edit')}}" icon="cpu">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+            </svg>
+            Skills
+        </x-admin.side-bar.nav-link>
+
+        <x-admin.side-bar.nav-link href="{{route('experience.edit')}}" icon="map">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z" />
+            </svg>
+            Experience
+        </x-admin.side-bar.nav-link>
 
         <div class="pt-10">
             <form action="{{ route('logout') }}" method="POST">
